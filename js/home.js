cca2d396561a410d452ec658118f7366a81d6340
Yalisson Queiroga <wicleyyalissonqueiroga@gmail.com>
2024-11-07 22:14:20 -0300

    Curso Conexão

---
+++ js/home.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+  AOS.init({
+    duration:1000
+});
